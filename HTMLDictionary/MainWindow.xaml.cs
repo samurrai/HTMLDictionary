a81281c5dc56e3d22419396da821be86fc6b8aa0
samurrai <samurrrai.r@gmail.com>
2019-06-27 13:03:26 +0600

    initial

---
+++ HTMLDictionary/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace HTMLDictionary
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(urlTextBox.Text))
+            {
+                MessageBox.Show("Введите URL сайта");
+                return;
+            }
+            try
+            {
+                HttpWebRequest request = WebRequest.CreateHttp(urlTextBox.Text);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    HtmlParser parser = new HtmlParser();
+                    var doc = parser.ParseDocument(reader.ReadToEnd());
+                    string str = "";
+
+                    foreach (HtmlElement node in doc.QuerySelectorAll("*"))
+                    {
+                        if (node.NodeValue != null)
+                        {
+                            str += node.NodeValue + ", ";
+                        }
+                    }
+                    mainTextBlock.Text = str;
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
